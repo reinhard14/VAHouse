0098d2cd14c078a8fdf5edbc04d67f9c25dc45b8
reinhard14 <gendrax_angcan@yahoo.com>
2024-06-13 15:12:18 +0800

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -151,7 +151,7 @@
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="rate"><span class="text-danger">*</span> Happy rate: (In pesos) </label>
+                                    <label for="rate"><span class="text-danger">*</span> Happy rate: <strong>(In pesos)</strong> </label>
                                     <input name="rate" type="text" class="form-control" required>
                                 </div>
 
