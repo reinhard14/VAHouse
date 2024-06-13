b28f13230305d8b26bfbf3d3389d5c69732979c2
reinhard14 <gendrax_angcan@yahoo.com>
2024-06-13 14:26:58 +0800

    fix the grid for create for administrator

--- resources/views/administrator/create.blade.php
+++ resources/views/administrator/create.blade.php
@@ -35,55 +35,84 @@
                         <form method="post" action="{{ route('administrator.store') }}" id="routeAdminForm">
                             @csrf
                             <input type="hidden" name="saving_option" id="savingOption" value="">
-                            <div class="form-group">
-                                <label for="name">First Name</label>
-                                <input type="text" class="form-control" placeholder="First name.." name="name" value="{{ old('name') }}" required>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="name">First Name</label>
+                                        <input type="text" class="form-control" placeholder="First name.." name="name" value="{{ old('name') }}" required>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="lastname">Last Name</label>
-                                <input type="text" class="form-control" placeholder="Last name.." name="lastname" value="{{ old('lastname') }}" required>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="lastname">Last Name</label>
+                                        <input type="text" class="form-control" placeholder="Last name.." name="lastname" value="{{ old('lastname') }}" required>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="contactnumber">Contact Number</label>
-                                <input type="number" class="form-control" placeholder="Last name.." name="contactnumber" value="{{ old('contactnumber') }}" required>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="contactnumber">Contact Number</label>
+                                        <input type="number" class="form-control" placeholder="Last name.." name="contactnumber" value="{{ old('contactnumber') }}" required>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="department">Department</label>
-                                <select class="form-control" id="department" name="department" {{ $departments->isEmpty() ? 'disabled' : ''}}>
-                                    @foreach ($departments as $department)
-                                        <option value="{{ $department->name }}">{{ $department->name }}</option>
-                                    @endforeach
-                                </select>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="department">Department</label>
+                                        <select class="form-control" id="department" name="department" {{ $departments->isEmpty() ? 'disabled' : ''}}>
+                                            @foreach ($departments as $department)
+                                                <option value="{{ $department->name }}">{{ $department->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="position">Position</label>
-                                <input type="text" class="form-control" placeholder="Enter position" name="position" value="{{ old('position') }}" required>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="position">Position</label>
+                                        <input type="text" class="form-control" placeholder="Enter position" name="position" value="{{ old('position') }}" required>
+                                    </div>
+                                </div>
                             </div>
 
                             <hr class="alert-info mt-4">
                             <small class="p1 mb-1"> Account login Details: </small>
-
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="text" class="form-control" placeholder="Email address.." name="email" value="{{ old('email') }}" required autofocus>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" placeholder="Password.." name="password" data-toggle="password" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="gender">Gender</label>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="gender" value="Male" required>
-                                    <label class="form-check-label">Male</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="gender" value="Female">
-                                    <label class="form-check-label">Female</label>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="text" class="form-control" placeholder="Email address.." name="email" value="{{ old('email') }}" required autofocus>
+                                    </div>
                                 </div>
                             </div>
-
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" placeholder="Password.." name="password" data-toggle="password" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="gender">Gender</label>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="gender" value="Male" required>
+                                            <label class="form-check-label">Male</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="gender" value="Female">
+                                            <label class="form-check-label">Female</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="d-flex justify-content-center">
                                 <button type="submit" class="btn btn-primary mr-2"><ion-icon name="navigate-outline" class="mr-1"></ion-icon>Submit</button>
                                 <a href="#" id="resetFieldButton" class="btn btn-outline-danger mr-2"><ion-icon name="backspace-outline" class="mr-1"></ion-icon>Reset Field</a>
