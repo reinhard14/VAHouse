a3c205114f532050c69af0f365a9cf5afddd828d
reinhard14 <gendrax_angcan@yahoo.com>
2024-06-29 00:02:34 +0800

    ADMIN SIDE - update edit components, making sure select tags are populated properly.

--- resources/views/components/administrator-applicant/edit.blade.php
+++ resources/views/components/administrator-applicant/edit.blade.php
@@ -19,34 +19,34 @@
                     <label class="form-label" for="lastname">Last Name </label>
                     <input class="form-control mb-2" type="text" name="lastname" value="{{ $user->lastname }}" required>
                     <label class="form-label" for="age">Age </label>
-                    <input class="form-control mb-2" type="number" name="age" required>
+                    <input class="form-control mb-2" type="number" name="age" value="{{ $user->age }}" required>
 
                     <label class="form-label" for="gender">Gender </label>
                     <select name="gender" class="form-control mb-2">
-                        <option value="Male" {{ old('gender', $user->gender ?? '') == 'Male' ? 'selected disabled' : '' }}>Male</option>
-                        <option value="Female" {{ old('gender', $user->gender ?? '') == 'Female' ? 'selected disabled' : '' }}>Female</option>
-                        <option value="Transgender" {{ old('gender', $user->gender ?? '') == 'Transgender' ? 'selected disabled' : '' }}>Transgender</option>
-                        <option value="Non binary" {{ old('gender', $user->gender ?? '') == 'Non-Binary/Non-Conforming' ? 'selected disabled' : '' }}>Non-Binary/Non-Conforming</option>
-                        <option value="Prefer not to respond" {{ old('gender', $user->gender ?? '') == 'Prefer not to respond' ? 'selected disabled' : '' }}>Prefer not to respond</option>
+                        <option value="Male" {{ old('gender', $user->gender ?? '') == 'Male' ? 'selected' : '' }}>Male</option>
+                        <option value="Female" {{ old('gender', $user->gender ?? '') == 'Female' ? 'selected' : '' }}>Female</option>
+                        <option value="Transgender" {{ old('gender', $user->gender ?? '') == 'Transgender' ? 'selected' : '' }}>Transgender</option>
+                        <option value="Non binary" {{ old('gender', $user->gender ?? '') == 'Non-Binary/Non-Conforming' ? 'selected' : '' }}>Non-Binary/Non-Conforming</option>
+                        <option value="Prefer not to respond" {{ old('gender', $user->gender ?? '') == 'Prefer not to respond' ? 'selected' : '' }}>Prefer not to respond</option>
                     </select>
 
                     <label class="form-label" for="education">Highest Educational Attainment </label>
                     <select name="education" class="form-control mb-2">
-                        <option value="High School">High School</option>
-                        <option value="Senior High School">Senior High School</option>
-                        <option value="College Undergrad">College Undergrad</option>
-                        <option value="College Degree">College Degree</option>
-                        <option value="Masters Degree">Master's Degree</option>
-                        <option value="Professional Degree">Professional Degree</option>
-                        <option value="Doctorate Degree">Doctorate Degree</option>
-                        <option value="Vocational">Vocational</option>
+                        <option value="High School" {{ old('education', $user->education ?? '') == 'High School' ? 'selected' : '' }}>High School</option>
+                        <option value="Senior High School" {{ old('education', $user->education ?? '') == 'Senior High School' ? 'selected' : '' }}>Senior High School</option>
+                        <option value="College Undergrad" {{ old('education', $user->education ?? '') == 'College Undergrad' ? 'selected' : '' }}>College Undergrad</option>
+                        <option value="College Degree" {{ old('education', $user->education ?? '') == 'College Degree' ? 'selected' : '' }}>College Degree</option>
+                        <option value="Masters Degree" {{ old('education', $user->education ?? '') == 'Master\'s Degree' ? 'selected' : '' }}>Master's Degree</option>
+                        <option value="Professional Degree" {{ old('education', $user->education ?? '') == 'Professional Degree' ? 'selected' : '' }}>Professional Degree</option>
+                        <option value="Doctorate Degree" {{ old('education', $user->education ?? '') == 'Doctorate Degree' ? 'selected' : '' }}>Doctorate Degree</option>
+                        <option value="Vocational" {{ old('education', $user->education ?? '') == 'Vocational' ? 'selected' : '' }}>Vocational</option>
                     </select>
 
                     <label class="form-label" for="email">Email Address </label>
                     <input class="form-control mb-2" type="email" name="email" value="{{ $user->email }}" required>
 
                     <label class="form-label" for="address">Address</label>
-                    <input class="form-control mb-2" type="text" name="address" required>
+                    <input class="form-control mb-2" type="text" name="address" value="{{ $user->address }}" required>
 
                     <label class="form-label" for="contactnumber">Contact Number</label>
                     <input class="form-control mb-2" type="number" value="{{ $user->contactnumber }}" name="contactnumber" required>
