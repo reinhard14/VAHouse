f7163133d25ee9bb38dc12d6af470085a7d4d8e0
reinhard14 <gendrax_angcan@yahoo.com>
2024-07-20 14:42:27 +0800

    . remove auth and redirectbasedonroleID

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         //additional route middleware
-        'role.redirect' => \App\Http\Middleware\RedirectBasedOnRoleId::class,
         'is_admin' => \App\Http\Middleware\IsAdminMiddleware::class,
     ];
 }
