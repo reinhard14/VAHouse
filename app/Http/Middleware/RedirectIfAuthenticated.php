b0811878c81e622f56ea00b00a2cc5e05902d35f
reinhard14 <gendrax_angcan@yahoo.com>
2024-07-20 16:01:07 +0800

    .

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,6 +20,7 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
+        $guards = empty($guards) ? [null] : $guards;
 
         if (Auth::guard($guard)->check()) {
             $user = Auth::user();
