2e6235fd016f6b93d7f00e7ee772316c16678c74
reinhard14 <gendrax_angcan@yahoo.com>
2024-08-16 16:30:16 +0800

    Bug Fix: Add confirmation on experience deletion.

--- public/dist/js/pages/applicants/edit-experience.js
+++ public/dist/js/pages/applicants/edit-experience.js
@@ -8,32 +8,36 @@ $(document).ready(function() {
         var formData = form.serialize();
         var csrfToken = $('meta[name="csrf-token"]').attr('content');
         var url = '/administrator/users/experiences/' + experienceId + '/delete';
-        console.log(experienceId);
-        $.ajax({
-            type: 'DELETE',
-            url: url,
-            data: formData,
-            headers: {
-                'X-CSRF-TOKEN': csrfToken
-            },
 
-            success: function(response) {
-                $('#tr_' + experienceId).remove();
-                deleteExperienceForm(response);
-            },
-            error: function(jqXHR) {
-                try {
-                    var responseJson = JSON.parse(jqXHR.responseText);
-                    var errorResponse = responseJson.errors
-                        ? Object.values(responseJson.errors).flat()
-                        : 'No errors found in the response';
+        var confirmation = confirm('Are you sure you want to delete this VA\'s experience?');
 
-                    var formattedResponse = JSON.stringify(errorResponse);
-                    handleFormWithMissingField(formattedResponse);
-                } catch (e) {
-                    alert('Invalid JSON response: ' + jqXHR.responseText);
+        if (confirmation) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                data: formData,
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken
+                },
+
+                success: function(response) {
+                    $('#tr_' + experienceId).remove();
+                    deleteExperienceForm(response);
+                },
+                error: function(jqXHR) {
+                    try {
+                        var responseJson = JSON.parse(jqXHR.responseText);
+                        var errorResponse = responseJson.errors
+                            ? Object.values(responseJson.errors).flat()
+                            : 'No errors found in the response';
+
+                        var formattedResponse = JSON.stringify(errorResponse);
+                        handleFormWithMissingField(formattedResponse);
+                    } catch (e) {
+                        alert('Invalid JSON response: ' + jqXHR.responseText);
+                    }
                 }
-            }
-        });
+            });
+        }
     });
 });
