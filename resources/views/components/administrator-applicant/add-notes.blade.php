056f21eddaf751b6fcb5c808f5d13dcea4383856
reinhard14 <gendrax_angcan@yahoo.com>
2024-08-20 16:15:40 +0800

    Bug fix: add aria-label in add notes modal.

--- resources/views/components/administrator-applicant/add-notes.blade.php
+++ resources/views/components/administrator-applicant/add-notes.blade.php
@@ -5,7 +5,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Add notes for this applicant</h5>
-                <button type="button" class="close" data-bs-dismiss="modal">x</button>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">x</button>
             </div>
 
             <form id="add-notes-form-{{ $user->id }}" data-user-id="{{ $user->id }}">
