94878af895e886f3dba0fa81a08b1b9df39c4274
reinhard14 <gendrax_angcan@yahoo.com>
2024-09-13 12:46:29 +0800

    re-commit remaining group by fields.

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -119,7 +119,8 @@ class AdminUserController extends Controller
                             ->groupBy(
                                 'users.id', 'users.name', 'users.lastname', 'users.contactnumber', 'users.email', 'users.email_verified_at',
                                 'users.age', 'users.education', 'users.address', 'users.middlename', 'users.suffix', 'users.password',
-                                'applicant_information.experience', 'users.created_at', 'users.updated_at', 'users.remember_token', 'users.role_id',
+                                'users.created_at', 'users.updated_at', 'users.remember_token', 'users.role_id', 'users.gender',
+                                'applicant_information.experience',
                                 'tiers.id',
                                 'tiers.tier'
                             )  // Add necessary fields to GROUP BY
@@ -146,10 +147,11 @@ class AdminUserController extends Controller
                             ->groupBy(
                                 'users.id', 'users.name', 'users.lastname', 'users.contactnumber', 'users.email', 'users.email_verified_at',
                                 'users.age', 'users.education', 'users.address', 'users.middlename', 'users.suffix', 'users.password',
-                                'applicant_information.experience', 'users.created_at', 'users.updated_at', 'users.remember_token', 'users.role_id',
+                                'users.created_at', 'users.updated_at', 'users.remember_token', 'users.role_id', 'users.gender',
+                                'applicant_information.experience',
                                 'tiers.id',
                                 'tiers.tier'
-                            )   // Add necessary fields to GROUP BY
+                            )  // Add necessary fields to GROUP BY
                             // ->groupBy('users.id')  // Group by user ID to ensure distinct users
                             //!test
                             // //distinct causes pagination error. find another way, try use groupby.
@@ -173,10 +175,11 @@ class AdminUserController extends Controller
                             ->groupBy(
                                 'users.id', 'users.name', 'users.lastname', 'users.contactnumber', 'users.email', 'users.email_verified_at',
                                 'users.age', 'users.education', 'users.address', 'users.middlename', 'users.suffix', 'users.password',
-                                'applicant_information.experience', 'users.created_at', 'users.updated_at', 'users.remember_token', 'users.role_id',
+                                'users.created_at', 'users.updated_at', 'users.remember_token', 'users.role_id', 'users.gender',
+                                'applicant_information.experience',
                                 'tiers.id',
                                 'tiers.tier'
-                            )   // Add necessary fields to GROUP BY
+                            )  // Add necessary fields to GROUP BY
                             // ->groupBy('users.id')  // Group by user ID to ensure distinct users
                             //!test
                             // //distinct causes pagination error. find another way, try use groupby.
