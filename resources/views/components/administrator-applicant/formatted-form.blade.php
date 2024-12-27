e2578c2987bf31e4c066c7950c21bd4dc3bb885a
reinhard14 <reinhardangcan@gmail.com>
2024-12-27 13:35:25 +0800

    BF: Text correction for Modal.

--- resources/views/components/administrator-applicant/formatted-form.blade.php
+++ resources/views/components/administrator-applicant/formatted-form.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog modal-dialog-scrollable modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Formatted Form</h5>
+                <h5 class="modal-title">VA Form</h5>
 
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">x</button>
             </div>
