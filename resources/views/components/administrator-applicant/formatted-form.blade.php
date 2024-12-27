9cac3204df82ab38b87c7212a9f057060fd7b4b8
reinhard14 <reinhardangcan@gmail.com>
2024-12-27 13:33:49 +0800

    BF: Fix modal size for generate form.

--- resources/views/components/administrator-applicant/formatted-form.blade.php
+++ resources/views/components/administrator-applicant/formatted-form.blade.php
@@ -1,10 +1,11 @@
 <!-- Formatted Form Modal -->
 
 <div class="modal fade" id="formatted-form-modal-{{ $user->id }}" tabindex="-1">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-dialog-scrollable modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Formatted Form</h5>
+
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">x</button>
             </div>
 
