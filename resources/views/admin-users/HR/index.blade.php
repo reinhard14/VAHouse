2626453236f0403b80a63f08196a39d996a7e81d
reinhard14 <reinhardangcan@gmail.com>
2025-06-09 14:45:33 +0800

    .

--- resources/views/admin-users/HR/index.blade.php
+++ resources/views/admin-users/HR/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper"">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header" >
         <div class="container-fluid" style="background-color: transparent;">
